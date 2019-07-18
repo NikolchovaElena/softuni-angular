7b73ac0ea731b97137445b2049eec17049e9b820
NikolchovaElena <elenanikolchova@gmail.com>
2019-07-18 21:36:10 +0300

    added template driven form validation

--- E2-forms/src/app/register-form/register-form.component.ts
+++ E2-forms/src/app/register-form/register-form.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
+  phoneNumberCodes: Array<string> = ['+359', '+721', '+115', '+302', '+709'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  register(data) {
+    console.log(data);
   }
 
 }
